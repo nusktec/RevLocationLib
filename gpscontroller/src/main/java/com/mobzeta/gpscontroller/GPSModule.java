67890a758c697876fd684c6bce419a0789d63d61
MobZeta <nusktecsoft@gmail.com>
2018-08-24 22:43:05 +0100

    Initial commit

---
+++ gpscontroller/src/main/java/com/mobzeta/gpscontroller/GPSModule.java
@@ -0,0 +1,16 @@
+package com.mobzeta.gpscontroller;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class GPSModule extends Activity {
+    private Context context;
+
+    public GPSModule(Context context) {
+        this.context = context;
+    }
+
+    public void getLongLat(){
+
+    }
+}
